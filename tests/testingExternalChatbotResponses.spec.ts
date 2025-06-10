16b95ff5700789c9e5ff6c15cdacf00d34ab674e
Stefano Falconi Caro <tepano2706@gmail.com>
2025-06-10 11:32:27 -0500

    Starting new project test

---
+++ tests/testingExternalChatbotResponses.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect, type Page } from '@playwright/test';
+
+const desiredLanguage = "Spanish"
+test.describe('External chatbot automated tests', () => { 
+
+  test.beforeEach(async ({ page }) => {
+  await page.goto('https://msoftpublicsite.z13.web.core.windows.net/#/home');
+  await page.locator('button #chat-icon').click()
+  const spanishButton = await page.getByRole('button').getByText('Español')
+  const englishButton = await page.getByRole('button').getByText('English')
+
+  await page.waitForTimeout(5000)
+
+  expect(englishButton).toBeVisible()
+  expect(spanishButton).toBeVisible()
+
+  if(desiredLanguage == "Spanish"){
+    await spanishButton.click()
+  }else if(desiredLanguage == "English"){
+    await englishButton.click()
+  }
+  });
+
+  test('Languages verification', async ({ page }) => {
+
+    const spanishButton = await page.getByRole('button').getByText('Español')
+    const englishButton = await page.getByRole('button').getByText('English')
+  });
+
+ 
+})
